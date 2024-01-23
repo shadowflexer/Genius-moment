7fc30948872a73d5314054200550487471741505
shadowflexer <145156649+shadowflexer@users.noreply.github.com>
2024-01-23 22:24:51 +0300

    Create main.py and we are molodec
    WE ARE MOLODEC

---
+++ main.py
@@ -0,0 +1,83 @@
+variables = {}
+
+
+def get_instruction(code_line: str) -> dict:
+    instruction = {}
+
+    # Если в строке кода содержится оператор присваивания
+    if ":=" in code_line:
+        # Разрезаем строку кода по пробелам
+        # Затем извлекаем из неё имя переменной и значение
+        components = code_line.split(" ")
+        variable_name = components[0]
+        value = int(components[2])
+        instruction = {"operation": "assign", "variable_name": variable_name, "value": value}
+    elif code_line[0:3] == "div":
+        components = code_line.split(" ")
+        variable_name = components[1]
+        podelit_na = components[2]
+        instruction = {"operation": "div", "variable_name": variable_name, "podelit_na": podelit_na}
+
+    elif code_line[0:3] == "sub":
+        components = code_line.split(" ")
+        variable_name = components[1]
+        minus = components[2]
+        instruction = {"operation": "sub", "variable_name": variable_name, "minus": minus}
+
+    elif code_line[0:3] == "add":
+        components = code_line.split(" ")
+        variable_name = components[1]
+        addend = components[2]
+        instruction = {"operation": "add", "variable_name": variable_name, "addend": addend}
+
+    elif code_line[0:3] == "mul":
+        components = code_line.split(" ")
+        variable_name = components[1]
+        multi = components[2]
+        instruction = {"operation": "mul", "variable_name": variable_name, "multi": multi}
+
+    return instruction
+
+
+def execute(code_line: str) -> None:
+    instruction = get_instruction(code_line)
+
+    # Если операция - присваивание
+    if instruction["operation"] == "assign":
+        # Достаём из массива имя переменной и значение
+        variable_name = instruction["variable_name"]
+        value = instruction["value"]
+
+        # Обращаемся к глобальному словарю переменных, 
+        # записываем по имени переменной соответствующее значение
+        variables[variable_name] = value
+    elif instruction["operation"] == "div":
+        variable_name = instruction["variable_name"]
+        podelit_na = instruction["podelit_na"]
+        variables[variable_name] = int(variables[variable_name]) // int(podelit_na)
+    
+    elif instruction["operation"] == "sub":
+        variable_name = instruction["variable_name"]
+        minus = instruction["minus"]
+        variables[variable_name] = int(variables[variable_name]) - int(minus)
+
+    elif instruction["operation"] == "add":
+        # Достаём из массива имя переменной и значение слагаемого
+        variable_name = instruction["variable_name"]
+        # Просим у пользователя число >>>
+        addend = instruction["addend"]
+        # Операция сложения >>> 
+        variables[variable_name] = int(variables[variable_name]) + int(addend)
+
+    elif instruction["operation"] == "mul":
+        variable_name = instruction["variable_name"]
+        mul = instruction["multi"]
+        variables[variable_name] = int(variables[variable_name]) * int(mul)
+
+
+if __name__ == '__main__':
+    print("Вас приветствует консоль! Удивительно, не правда ли? Вводите команды.")
+    # Бесконечно просим пользователя вводить команды и исполняем их
+    while True:
+        command = input(">>> ")
+        execute(command)
